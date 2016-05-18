c3fea0143a44a7a30e47492eef623e95772badb2
alvaro rodriguez <alvarogaston1997@hotmail.com>
2016-05-18 19:58:47 -0300

    calculadora 18/5

--- calculator.rb
+++ calculator.rb
@@ -1,32 +1,46 @@
+require_relative "sum"
+require_relative "difference"
+require_relative "multiplication"
+require_relative "division"
+require_relative "inverse"
+require_relative "square"
+require_relative "squareroot"
 class Calculator
   def add_operand(op_received)
-    @operand_temp = op_received
+    if @current_operation.nil?
+       @operand_temp = op_received
+     else
+       @current_operation.add_operand(op_received)
+    end
   end
 
-  def set_operation(Operation)
-    if Operation == "+"
-      Operation_klass = Sum
-    elsif Operation == "-"
-      Operation_klass = Difference
-    elsif Operation == "*"
-      Operation_klass = Multiplication
-    elsif Operation == "/"
-      Operation_klass = Division
-    elsif Operation == "INV"
-      Operation_klass = Inverse
-    elsif Operation == "SQ"
-      Operation_klass = Square
-    elsif Operation == "SQRT"
-      Operation_klass = Squareroot
-      
+  def set_operation(operation)
 
-
+      operation_klass =
+        case operation
+        when "+" then Sum
+        when "-" then Difference
+        when "*" then Multiplication
+        when "/" then Division
+        when "INV" then Inverse
+        when "SQ" then Square
+        when "SQRT" then Squareroot
+        end
+    @current_operation = operation_klass.new(@operand_temp)
+    @operand_temp = nil
   end
 
-  def execute()
+  def execute
+    result = @current_operation.execute
+    clear
+    result
+    rescue ZeroDivisionError
+      'ERROR'
   end
 
-  def clear()
+  def clear
+    @current_operation = nil
+    @operand_temp = nil
   end
 
 end
